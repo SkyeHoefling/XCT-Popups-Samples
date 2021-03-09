f178c6ec1c38165349637a233e6f8f927bf06bd1
Andrew Hoefling <andrewh@fileonq.com>
2021-03-08 22:07:41 -0500

    Added XCT Popup Sample

---
+++ src/PopupSample/HelloPopup.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PopupSample
+{
+    public partial class HelloPopup
+    {
+        public HelloPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/PopupSample/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace PopupSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.ShowPopup(new HelloPopup());
+        }
+    }
+}
